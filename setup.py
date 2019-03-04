9bab41d1dac6e9189e6bdf49a7ace1ea84511888
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2019-03-04 20:47:27 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
 	name='anna_tasks',
-	version='1.0.0',
+	version='1.0.1',
 	author='Patrik Pihlstrom',
 	author_email='patrik.pihlstrom@gmail.com',
 	url='https://github.com/patrikpihlstrom/anna-tasks',
